eedc04068a4701b1723276eda3dcf888e0583bcf
Justin Daniel <justinddaniel@gmail.com>
2017-09-26 19:04:57 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,14 +1,14 @@
 require 'pry'
 class EmailParser
   @@all = []
-  
+
   attr_accessor :emails
 
   def initialize(emails)
     @emails = emails
   end
 
-  def self.parse
+  def parse
     new_emails = emails.scan(/\S+@+[^,||^\s]{1,100}/)
     new_emails = new_emails.uniq
     new_emails
