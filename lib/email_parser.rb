9de70bf953362137b039e7839a14dab893cdd01a
Justin Daniel <justinddaniel@gmail.com>
2017-09-26 19:06:48 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,7 +9,7 @@ class EmailParser
   end
 
   def parse
-    new_emails = emails.scan(/\S+@+[^,||^\s]{1,100}/)
+    new_emails = self.emails.scan(/\S+@+[^,||^\s]{1,100}/)
     new_emails = new_emails.uniq
     new_emails
   end
