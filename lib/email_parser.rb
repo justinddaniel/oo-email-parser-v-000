7907e8a1a9289eb022528eb916bbee910c6ad8ac
Justin Daniel <justinddaniel@gmail.com>
2017-09-26 18:35:38 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,7 +8,7 @@ class EmailParser
 
   attr_accessor :emails
 
-  def self.parse(emails)
+  def self.parse
     new_emails = emails.scan(/\S+@+[^,||^\s]{1,100}/)
     new_emails = new_emails.uniq
     new_emails.each { |e| @@all << e }
